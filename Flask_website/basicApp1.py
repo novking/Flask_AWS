d9de57b363383350c3f910d2b8486b8b9fbc09e6
Yiming Zhang <awesome1aaron@gmail.com>
2016-06-29 22:46:37 -0400

    registration
    registration

--- Flask_website/basicApp1.py
+++ Flask_website/basicApp1.py
@@ -1,4 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, url_for, redirect, session
+from dbconnect import connection
+
+from wtforms import Form, BooleanField, TextField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from MySQLdb import escape_string as thwart
+import gc
 
 app = Flask(__name__)
 
@@ -29,5 +35,69 @@ def profile(name):
 def shopping():
     food = ["chesscake", "beef", "indian buffet", "Chinese!"]
     return render_template('shopping.html', food= food)
+
+
+@app.route('/register', methods=["GET","POST"])
+def register_page():
+    try:
+        c, conn = connection()
+        return("okay")
+    except Exception as e:
+        return(str(e))
+
+
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=20)])
+    email = TextField('Email Address', [validators.Length(min=6, max=50)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)', [validators.Required()])
+
+
+@app.route('/register1', methods=["GET","POST"])
+def register_page():
+    try:
+        form = RegistrationForm(request.form)
+
+        if request.method == "POST" and form.validate():
+            username  = form.username.data
+            email = form.email.data
+            password = sha256_crypt.encrypt((str(form.password.data)))
+            c, conn = connection()
+
+            x = c.execute("SELECT * FROM users WHERE username = (%s)",
+                          (thwart(username)))
+
+            if int(x) > 0:
+                flash("That username is already taken, please choose another")
+                return render_template('register.html', form=form)
+
+            else:
+                c.execute("INSERT INTO users (username, password, email, tracking) VALUES (%s, %s, %s, %s)",
+                          (thwart(username), thwart(password), thwart(email), thwart("/introduction-to-python-programming/")))
+
+                conn.commit()
+                flash("Thanks for registering!")
+                c.close()
+                conn.close()
+                gc.collect()
+
+                session['logged_in'] = True
+                session['username'] = username
+
+                return redirect(url_for('dashboard'))
+
+        return render_template("register.html", form=form)
+
+    except Exception as e:
+        return(str(e))
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
