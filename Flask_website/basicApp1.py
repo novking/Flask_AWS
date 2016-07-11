5f3e32b66596be35dab52f666c6b66ce01785b84
Yiming Zhang <awesome1aaron@gmail.com>
2016-07-10 22:04:02 -0400

    "callback" on Flask
    very interesting practice on how to set the lang of the user when he/she
    visiting my website. use the decorator to do that. good to keep in mind

--- Flask_website/basicApp1.py
+++ Flask_website/basicApp1.py
@@ -96,8 +96,21 @@ def register_page():
         return(str(e))
 
 
+def after_this_request(f):
+    if not hasattr(g, 'after_request_callbacks'):
+        g.after_request_callbacks = []
+    g.after_request_callbacks.append(f)
+    return f
 
-
+@app.before_request
+def detect_user_language():
+    language = request.cookies.get('user_lang')
+    if language is None:
+        language = guess_language_from_request()
+        @after_this_request
+        def remember_language(response):
+            response.set_cookie('user_lang', language)
+    g.language = language
 
 if __name__ == '__main__':
     app.run(debug=True)
